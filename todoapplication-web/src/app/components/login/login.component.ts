3f7e96d35d3f0a71c8daa33b32b6b8a74199df7f
Marco Carleti <marcarleti@gmail.com>
2024-01-18 15:22:01 -0300

    feat: add login function to login component

--- todoapplication-web/src/app/components/login/login.component.ts
+++ todoapplication-web/src/app/components/login/login.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { BouncingLogoComponent } from '../bouncing-logo/bouncing-logo.component';
+import { JwtService } from '../../service/jwt.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  standalone: true,
+  imports: [ReactiveFormsModule, BouncingLogoComponent],
+  styleUrl: './login.component.css',
+  providers: [JwtService],
 })
-export class LoginComponent {
-  title = "login"
-  imageSrc = "assets/ToDo-Logo.svg"
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup | any;
+
+  constructor(private service: JwtService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  submitForm() {
+    console.log(this.loginForm.value);
+    this.service.login(this.loginForm.value).subscribe((response) => {
+      if (response.jwt != null) {
+        alert('Hello, your token is ' + response.jwt);
+        const jwtToken = response.jwt;
+        localStorage.setItem('jwt', jwtToken);
+      }
+    });
+  }
 }
