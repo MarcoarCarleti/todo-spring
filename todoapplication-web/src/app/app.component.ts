b7b2ac64f127e82a3601086701061238aadbe299
Marco Carleti <marcarleti@gmail.com>
2024-01-18 13:48:22 -0300

    feat: validated register form

--- todoapplication-web/src/app/app.component.ts
+++ todoapplication-web/src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoapplication-web';
-  
-
-  
 }
--- todoapplication-web/src/app/components/register/register.component.ts
+++ todoapplication-web/src/app/components/register/register.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BouncingLogoComponent } from '../bouncing-logo/bouncing-logo.component';
+import { JwtService } from '../../service/jwt.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [BouncingLogoComponent, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
-  imageSrc = "assets/ToDo-Logo.svg"
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private service: JwtService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMathValidator }
+    );
+  }
+
+  passwordMathValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      formGroup.get('confirmPassword')?.setErrors({
+        passwordMismatch: true,
+      });
+    } else {
+      formGroup.get('confirmPassword')?.setErrors(null);
+    }
+  }
+
+  submitForm() {}
 }
