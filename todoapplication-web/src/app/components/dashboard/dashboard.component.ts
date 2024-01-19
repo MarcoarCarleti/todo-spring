9d18b2963e12f16cbcecd2ab793742ed05905dae
Marco Carleti <marcarleti@gmail.com>
2024-01-19 15:08:17 -0300

    feat: created dashboard component

--- todoapplication-web/src/app/components/dashboard/dashboard.component.ts
+++ todoapplication-web/src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { JwtService } from '../../service/jwt.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { JwtService } from '../../service/jwt/jwt.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from "../header/header.component";
+import {
+  NgbAccordionModule,
+  NgbModal,
+  NgbModalModule,
+} from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from '../header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BsModalService, BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
+import { EditDialogComponent } from '../../shared/modal/edit-dialog/edit-dialog.component';
+import { AddDialogComponent } from '../../shared/modal/add-dialog/add-dialog/add-dialog.component';
 
 interface Tasks {
   id: number;
@@ -13,33 +21,72 @@ interface Tasks {
 }
 
 @Component({
-    selector: 'app-dashboard',
-    standalone: true,
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.css',
-    providers: [JwtService],
-    imports: [CommonModule, NgbAccordionModule, HeaderComponent]
+  selector: 'app-dashboard',
+  standalone: true,
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+  providers: [JwtService, BsModalService],
+  imports: [
+    CommonModule,
+    NgbAccordionModule,
+    HeaderComponent,
+    MatIconModule,
+    ModalModule,
+    EditDialogComponent,
+    NgbModalModule,
+  ],
 })
 export class DashboardComponent implements OnInit {
-  constructor(private service: JwtService) {}
   tasks!: Tasks[];
+  bsModalRef: BsModalRef | undefined;
+  taskDone: boolean | undefined;
+
+  constructor(private service: JwtService, private modalService: NgbModal) {}
 
   ngOnInit(): void {
     this.getTask();
-
-    setTimeout(() => {
-      console.log(this.tasks);
-    }, 3000);
   }
 
   getTask() {
     this.service.task().subscribe((res) => {
-      console.log(res);
       this.tasks = res;
     });
   }
 
-  // openAccordion() {
-  //   this.button.coll
-  // }
+  handleDeleteClick(taskId: number) {
+    this.service.deleteTask(taskId).subscribe(() => {
+      this.getTask();
+    });
+  }
+
+  handleEditClick(task: Tasks) {
+    const modalRef = this.modalService.open(EditDialogComponent, {
+      backdrop: true,
+    });
+    modalRef.componentInstance.task = task;
+  }
+
+  handleUpdateTaskDone(task: Tasks) {
+    const taskObject = {
+      title: task.title,
+      description: task.description,
+      done: !task.done,
+    };
+
+    this.taskDone = taskObject.done;
+
+    this.service.updateTask(task.id, taskObject).subscribe(() => {
+      this.getTask();
+    });
+  }
+
+  handleAddClick() {
+    this.modalService.open(AddDialogComponent, {
+      backdrop: true,
+    });
+  }
 }
+
+// openAccordion() {
+//   this.button.coll
+// }
